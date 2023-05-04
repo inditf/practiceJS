d829fa2752e461ee088ed61cbca57f40848d349d
name <inditif@icloud.com>
2023-05-04 16:19:04 +0800

    update 10

---
+++ 10DOM_Task_CommentBoard/script.js
@@ -0,0 +1,41 @@
+
+//留言数据对象
+const comments = [
+  {
+    name: 'Danny',
+    comment: 'Lorem Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+    time: 'Thu Jan 12 2022'
+  },
+  {
+    name: 'Jackson',
+    comment: 'Lorem Ipsum dolor sit amet, consectetur adipiscing elit',
+    time: 'Thu Jan 11 2022'
+  }
+];
+
+/**
+ * @description 基于comments数据，完成渲染留言板渲染
+ * @param {object[]} comments
+ */
+const renderComments = (comments) => {
+  // your code here
+}
+
+// 调用
+renderComments();
+
+/**
+ * @description 基于comments数据，完成添加一条留言，注意安全问题
+ * @param {object[]} comments
+ */
+const postComment = (comments) => {
+  // your code here
+}
+
+
+/**
+ * @description 切换关闭留言和开启留言两种模式
+ */
+const toggleComment = () => {
+  // your code here 
+}
