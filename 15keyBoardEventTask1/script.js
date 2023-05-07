c9bbe7de49d3133acfe8d9dc3ed2781fc4ec84de
name <inditif@icloud.com>
2023-05-07 10:04:42 +0800

    update 15Task1

--- 15keyBoardEventTask1/script.js
+++ 15keyBoardEventTask1/script.js
@@ -20,3 +20,14 @@ search.addEventListener('keyup', (event) => {
     }
 
 });
+
+// 自测问题
+// 1.event.key 和 event.code 有什么区别？
+// event.key是按下的键的字符，event.code是按下的键的键码
+// 2.当按下键盘时，会连续触发多个事件，说说是哪些事件，她们是按什么顺序触发？
+// keydown, keypress, keyup
+// 3.如果按下字符键不放，哪两个事件会被一直触发？
+// keydown, keypress
+// 4.如果按下非字符键不放，有几个事件会被一直触发？是哪些事件？
+// 1个，keydown
+
