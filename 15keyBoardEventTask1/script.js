f32be9ae07dffb051376bf1b245034399f0efcb0
name <inditif@icloud.com>
2023-05-07 09:58:31 +0800

    update 15Task1

--- 15keyBoardEventTask1/script.js
+++ 15keyBoardEventTask1/script.js
@@ -1,23 +1,22 @@
 const search = document.querySelector('#search');
 const phonebook = document.querySelector('#phonebook');
 
-search.addEventListener('input', () => {
-    const term = this.value.toLowerCase();
-    const items = phonebook.querySelectorAll('li');
-    Array.from(items).forEach(function (item) {
-        const name = item.textContent;
-        if (name.toLowerCase().indexOf(term) != -1) {
-            item.style.display = 'block';
-        } else {
-            item.style.display = 'none';
-        }
-    });
-});
-
-function checkKey(e) {
-    if (e.keyCode >= 48 && e.keyCode <= 57 || e.keyCode >= 65 && e.keyCode <= 90 || e.keyCode >= 37 && e.keyCode <= 40 || e.keyCode == 8) {
-        return true;
-    } else {
-        return false;
+search.addEventListener('keyup', (event) => {
+    if (event.keyCode >= 48 && event.keyCode <= 57 || event.keyCode >= 65 && event.keyCode <= 90 || event.keyCode >= 37 && event.keyCode <= 40 || event.keyCode == 8) {
+        const term = event.target.value.toLowerCase();
+        const items = phonebook.querySelectorAll('li');
+        Array.from(items).forEach(item => {
+            const name = item.textContent;
+            if (name.toLowerCase().indexOf(term) != -1) {
+                item.style.display = 'block';
+            } else {
+                item.style.display = 'none';
+            }
+        });
     }
-}
+    else {
+        event.target.value = "";
+        alert("Please enter a valid character");
+    }
+
+});
