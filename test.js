c7b6134b3ad5f65ea45896c6d46427cb0dcbff64
name <inditif@icloud.com>
2023-06-07 09:55:18 +0800

    update WIP-2

--- test.js
+++ test.js
@@ -1,14 +1,11 @@
-function useEffect(callback, depArray) {
-    const hasNoDeps = !depArray;  // 检查是否提供了依赖数组
-    const deps = depArray || [];  // 如果没有提供依赖数组，则使用空数组
-    const hasChangedDeps = deps ? !deps.every((el, i) => el === depArray[i]) : true;
-    // 如果没有提供依赖数组或依赖数组中的元素发生了变化，则执行回调函数
-    if (hasNoDeps || hasChangedDeps) {
-        callback();
-    }
+async function f() {
+    let promise = new Promise((resolve, reject) => {
+        console.log("promise started");
+        setTimeout(() => resolve("done!"), 1000)
+    });
+    console.log("before await");
+    let result = await promise;
+    console.log(result);
 }
-//test
-useEffect(() => {
-    console.log('useEffect');
-},)
-//count发生变化时，才会执行回调函数
+
+f();
